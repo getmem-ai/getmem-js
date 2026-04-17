910038dedb0e43baad638d5088f4b1f7041fbd03
NimbleV2023 <133707915+NimbleV2023@users.noreply.github.com>
2026-04-17 14:26:20 +0400

    build: compiled JS v0.1.1

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 export { GetMem, GetMemError } from './client';
+export { APIError, UnauthorizedError, QuotaExceededError, ForbiddenError, NotFoundError, ValidationError, RateLimitedError, InternalError, ServiceUnavailableError, ConnectionError, TimeoutError, } from './errors';
 //# sourceMappingURL=index.js.map
