c1911e4532fbfff1fb23b007776507caaba36f1e
NimbleV2023 <133707915+NimbleV2023@users.noreply.github.com>
2026-04-17 14:26:24 +0400

    build: type definitions v0.1.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { GetMem, GetMemError } from './client';
+export { APIError, UnauthorizedError, QuotaExceededError, ForbiddenError, NotFoundError, ValidationError, RateLimitedError, InternalError, ServiceUnavailableError, ConnectionError, TimeoutError, } from './errors';
 export type { GetMemConfig, IngestRequest, IngestResponse, GetRequest, GetResponse, MemoryItem, ContextMeta, HealthResponse, MessageIn, } from './types';
 //# sourceMappingURL=index.d.ts.map
